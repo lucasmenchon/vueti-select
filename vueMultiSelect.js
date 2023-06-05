a0fde88dfea88582ba891a4db1506bb4f1c79057
lucasmenchon <it.lucas@outlook.com>
2023-06-05 18:06:40 -0300

    implement font awesome, need better design

---
+++ vueMultiSelect.js
@@ -0,0 +1,51 @@
+var VueMultiSelect = Vue.component('VueMultiSelect', {
+  template: `
+  <div id="VueMultiSelect" class="dropdown">
+  <button class="btn btn-default dropdown-toggle form-control" type="button" @click="toggleDropdown">
+    {{ selectedOptions.length === 0 ? 'Nenhuma opção selecionada' : selectedOptions.join(', ') }}
+    <span class="caret"></span>
+  </button>
+  <ul v-show="showDropdown" class="dropdown-menu">
+    <li class="search-bar">
+      <div class="input-group">
+        <input type="text" v-model="searchTerm" placeholder="Pesquisar" class="form-control" />
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary clear-button" type="button" @click="clearSearch">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+    </li>
+    <li v-for="option in filteredOptions">
+      <label class="checkbox-label">
+        <input type="checkbox" v-model="selectedOptions" :value="option" class="form-check-input" />
+        {{ option }}
+      </label>
+    </li>
+  </ul>
+</div>
+  `,
+  data() {
+    return {
+      options: ['teste1', 'teste2', 'teste3'],
+      selectedOptions: [],
+      searchTerm: '',
+      showDropdown: false
+    }
+  },
+  computed: {
+    filteredOptions() {
+      return this.options.filter(option =>
+        option.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+    },
+    clearSearch() {
+      this.searchTerm = '';
+    }
+  }
+});
