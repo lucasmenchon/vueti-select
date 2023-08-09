74d795e6c9682927eaf4516eed87e19dbef09bbd
lucasmenchon <developer@luccas.dev>
2023-08-09 01:36:56 -0300

    added new icons, license, transition effect when opening select, code improvements, design improvements.

--- vueMultiSelect.js
+++ vueMultiSelect.js
@@ -1,18 +1,22 @@
-var VueMultiSelect = Vue.component('VueMultiSelect', {
-  template:`
+var VueMultiSelect = Vue.component("VueMultiSelect", {
+  template: `
 <div id="VueMultiSelect" class="vuetiSelectBox">
-    <div :class="cssBtnBox.customBtn" @click="toggleDropdown()" @mouseenter="changeIconBtn(true)"
-    @mouseleave="changeIconBtn(false)">
-    <span>
-      {{ selectedOptions.length === 0 ? 'Nenhuma opção selecionada' : selectedOptions.join(', ') }}      
-    </span>
-    <i class="svgArrow arrowIcon" :class="cssBtnBox.icon"></i>
+    <div class="buttonBox">
+    <button type="button" :class="cssBtnBox.customBtn"
+    @click="toggleDropdown()">
+    <span>{{ selectedOptions.length === 0 ? 'Nenhuma opção selecionada' : selectedOptions.join(', ') }}</span>    
+    <svg xmlns="http://www.w3.org/2000/svg"
+    fill="#000" viewBox="0 0 320 512"
+    class="svgCaretDown caretDownIcon" :class="cssBtnBox.icon">
+    <pathd="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+    </svg>    
     </div>
-    <div v-show="showDropdownMenu" :class="cssDropdownMenu">
+    <div  :class="cssDropdownMenu">
+     <div v-show="showDropdownMenu">
       <div class="searchBox">
-        <button class="searchIcon" type="button">
-          <i class="svgSearch"></i>
-        </button>
+        <div class="searchIcon">
+          <i class="svgSearch"></i>          
+        </div>
         <input type="text" v-model="searchTerm" placeholder="Pesquisar" class="searchBar" />
         <button class="clearButton" type="button" @click="clearSearch">
           <i class="svgEraser"></i>
@@ -45,48 +49,114 @@ var VueMultiSelect = Vue.component('VueMultiSelect', {
       </div>
     </div>
   </div>
+</div>
   `,
 
   data() {
     return {
       groups: [
-        { name: 'Roupas', expanded: false, objects: ['Calça', 'Camisa', 'Casaco'] },
-        { name: 'Acessórios', expanded: false, objects: ['Óculos', 'Boné', 'Colar'] },
-        { name: 'Alimentos', expanded: false, objects: ['Frutas', 'Legumes', 'Carnes'] },
-        { name: 'Eletrônicos', expanded: false, objects: ['Smartphone', 'Notebook', 'Fones de Ouvido'] },
-        { name: 'Livros', expanded: false, objects: ['Ficção', 'Não Ficção', 'Autoajuda'] },
-        { name: 'Esportes', expanded: false, objects: ['Bola', 'Raquete', 'Tênis'] },
-        { name: 'Casa', expanded: false, objects: ['Sofá', 'Mesa', 'Cadeira'] },
-        { name: 'Beleza', expanded: false, objects: ['Maquiagem', 'Creme', 'Perfume'] },
-        { name: 'Brinquedos', expanded: false, objects: ['Boneca', 'Carrinho', 'Bola'] },
-        { name: 'Jogos', expanded: false, objects: ['Tabuleiro', 'Cartas', 'Videogame'] },
-        { name: 'Ferramentas', expanded: false, objects: ['Martelo', 'Chave de Fenda', 'Serra'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
-        { name: 'Viagens', expanded: false, objects: ['Passagem Aérea', 'Hotel', 'Mala'] },
+        {
+          name: "Roupas",
+          expanded: false,
+          objects: ["Calça", "Camisa", "Casaco"],
+        },
+        {
+          name: "Acessórios",
+          expanded: false,
+          objects: ["Óculos", "Boné", "Colar"],
+        },
+        {
+          name: "Alimentos",
+          expanded: false,
+          objects: ["Frutas", "Legumes", "Carnes"],
+        },
+        {
+          name: "Eletrônicos",
+          expanded: false,
+          objects: ["Smartphone", "Notebook", "Fones de Ouvido"],
+        },
+        {
+          name: "Livros",
+          expanded: false,
+          objects: ["Ficção", "Não Ficção", "Autoajuda"],
+        },
+        {
+          name: "Esportes",
+          expanded: false,
+          objects: ["Bola", "Raquete", "Tênis"],
+        },
+        { name: "Casa", expanded: false, objects: ["Sofá", "Mesa", "Cadeira"] },
+        {
+          name: "Beleza",
+          expanded: false,
+          objects: ["Maquiagem", "Creme", "Perfume"],
+        },
+        {
+          name: "Brinquedos",
+          expanded: false,
+          objects: ["Boneca", "Carrinho", "Bola"],
+        },
+        {
+          name: "Jogos",
+          expanded: false,
+          objects: ["Tabuleiro", "Cartas", "Videogame"],
+        },
+        {
+          name: "Ferramentas",
+          expanded: false,
+          objects: ["Martelo", "Chave de Fenda", "Serra"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
+        {
+          name: "Viagens",
+          expanded: false,
+          objects: ["Passagem Aérea", "Hotel", "Mala"],
+        },
       ],
       selectedOptions: [],
-      searchTerm: '',
+      searchTerm: "",
       showDropdownMenu: false,
       cssBtnBox: {
-        customBtn: 'close-btnVueti',
-        icon: 'close'
+        customBtn: "close-btnVueti",
+        icon: "close",
       },
-      cssDropdownBox: '',
-      cssDropdownMenu: '',
+      cssDropdownBox: "",
+      cssDropdownMenu: "dropdownMenu",
       isMouseOver: false,
-    }
+    };
   },
   computed: {
     filteredOptions() {
-      return this.groups.filter(group =>
-        group.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-        group.objects.some(object =>
-          object.toLowerCase().includes(this.searchTerm.toLowerCase())
-        )
+      return this.groups.filter(
+        (group) =>
+          group.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          group.objects.some((object) =>
+            object.toLowerCase().includes(this.searchTerm.toLowerCase())
+          )
       );
     },
   },
@@ -94,41 +164,38 @@ var VueMultiSelect = Vue.component('VueMultiSelect', {
     toggleDropdown() {
       this.showDropdownMenu = !this.showDropdownMenu;
       if (this.showDropdownMenu) {
-        this.cssDropdownMenu = 'dropdownMenu';
-        this.cssBtnBox.customBtn = 'open-btnVueti';
-        this.cssDropdownBox = 'dropdownBox';
-        document.addEventListener('click', this.handleOutsideClick);
+        this.cssDropdownMenu = "dropdownMenu active";
+        this.cssBtnBox.icon = "open";
+        this.cssBtnBox.customBtn = "open-btnVueti";
+        this.cssDropdownBox = "dropdownBox";
+        document.addEventListener("click", this.handleOutsideClick);
       } else {
-        this.cssBtnBox.customBtn = 'close-btnVueti';
-        this.cssDropdownBox = '';
-        document.removeEventListener('click', this.handleOutsideClick);
-      }
-    },
-    changeIconBtn(value) {
-      if (this.showDropdownMenu) {
-        this.cssBtnBox.icon = 'open';
-      } else {
-        this.cssBtnBox.icon = value ? 'open' : 'close';
+        this.cssDropdownMenu = "dropdownMenu";
+        this.cssBtnBox.icon = "close";
+        this.cssBtnBox.customBtn = "close-btnVueti";
+        this.cssDropdownBox = "";
+        document.removeEventListener("click", this.handleOutsideClick);
       }
     },
     clearSearch() {
-      this.searchTerm = '';
+      this.searchTerm = "";
     },
     toggleGroup(group) {
       group.expanded = !group.expanded;
     },
     handleOutsideClick(event) {
       if (this.showDropdownMenu && !this.$el.contains(event.target)) {
-        this.cssBtnBox.customBtn = 'close-btnVueti';
-        this.cssBtnBox.icon = 'close';
+        this.cssDropdownMenu = "dropdownMenu";
+        this.cssBtnBox.customBtn = "close-btnVueti";
+        this.cssBtnBox.icon = "close";
+        this.searchTerm = "";
+        this.cssDropdownBox = "";
         this.showDropdownMenu = false;
-        this.searchTerm = '';
-        this.cssDropdownBox = '';
-        document.removeEventListener('click', this.handleOutsideClick);
+        document.removeEventListener("click", this.handleOutsideClick);
       }
-    }
+    },
   },
   beforeDestroy() {
-    document.removeEventListener('click', this.handleOutsideClick);
-  }
+    document.removeEventListener("click", this.handleOutsideClick);
+  },
 });
