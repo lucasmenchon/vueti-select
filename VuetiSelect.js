d6fd7f0a13c79e446020c2009aa9c22b64982381
lucasmenchon <developer@luccas.dev>
2023-08-13 03:20:16 -0300

    design changes, add props with array value, examples now in index.html, code review

--- VuetiSelect.js
+++ VuetiSelect.js
@@ -1,118 +1,66 @@
 var VuetiSelect = Vue.component("VuetiSelect", {
   template: `
   <div id="VuetiSelect" class="vuetiSelectBox">
-  <div class="dropdownContainer">
-    <div class="buttonBox">
-      <button type="button" :class="cssBtnBox.customBtn" @click="toggleDropdown()">
-        <span>{{ selectedOptions.length === 0 ? 'Nenhuma opção selecionada' : selectedOptions.join(', ') }}</span>
-        <i :class="cssBtnBox.icon"></i>
-    </div>
-    <transition name="fade">
-      <div v-if="showDropdownMenu" :class="cssDropdownMenu">
-        <div class="searchBox">
-          <div class="svgSearchBox">
-            <i class="svgSearch"></i>
-          </div>
-          <input type="text" v-model="searchTerm" placeholder="Pesquisar" class="searchBar" />
-          <button class="clearButton" type="button" @click="clearSearch">
-            <i class="svgEraser"></i>
-          </button>
-        </div>
-        <div :class="cssUlBox">
-          <ul class="ulMenu">
-            <li>Selecionar tudo</li>
-            <li v-for="group in filteredOptions" :key="group.name">
-              <div class="">
-                <label class="">
-                  <input type="checkbox" v-model="selectedOptions" :value="group.name" />
-                  {{ group.name }}
-                </label>
-                <button type="button" @click="toggleGroup(group)" class="checkboxButton">
-                  <i :class="group.cssCheckbox"></i>
-                </button>
-              </div>
-              <ul v-show="group.expanded" class="ulMenu">
-                <li v-for="object in group.objects" :key="object">
-                  <label class="">
-                    <input type="checkbox" v-model="selectedOptions" :value="object" class="" />
-                    {{ object }}
-                  </label>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+    <div class="dropdownContainer">
+      <div class="buttonBox">
+        <button type="button" :class="cssBtnBox.customBtn" @click="toggleDropdown()">
+          <span>{{ selectedOptions.length === 0 ? 'No Option Selected' : selectedOptions.join(', ') }}</span>
+          <i :class="cssBtnBox.icon"></i>
+        </button>
       </div>
-    </transition>
+      <transition name="fade">
+        <div v-if="showDropdownMenu" :class="cssDropdownMenu">
+          <div class="searchBox">
+            <div class="svgSearchBox">
+              <i class="svgSearch"></i>
+            </div>
+            <input type="text" v-model="searchTerm" placeholder="Search" class="searchBar" />
+            <button class="clearButton" type="button" @click="clearSearch">
+              <i class="svgEraser"></i>
+            </button>
+          </div>
+          <div :class="cssUlBox">
+            <div class="boxSelectAll">
+              <label class="labelSelectAll">
+                <input type="checkbox" v-model="selectAll" id="no-margin" class="inputSelectAll" />
+                Select All
+              </label>
+            </div>
+            <ul class="ulMenu">
+              <li v-for="group in filteredOptions" class="groupItem">
+                <div class="groupBox">
+                  <input type="checkbox" id="no-margin" class="groupCheckbox" v-model="selectedOptions" :value="group.name" />
+                  <label class="groupLabel">
+                    {{ group.name }}
+                  <button type="button" @click="toggleGroup(group)" class="checkboxButton">
+                    <i :class="group.cssCheckbox"></i>
+                  </button>
+                  </label>
+                </div>
+                <transition name="fade">
+                  <ul v-show="group.expanded" class="groupObjects">
+                    <li v-for="object in group.objects" :key="object">
+                      <label class="">
+                        <input type="checkbox" v-model="selectedOptions" :value="object" />
+                        {{ object }}
+                      </label>
+                    </li>
+                  </ul>
+                </transition>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
-</div>
   `,
-
+  props: {
+    value: Array,    
+  },
   data() {
-    return {
-      groups: [
-        {
-          name: "Clothing",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Pants", "Shirt", "Jacket"],
-        },
-        {
-          name: "Accessories",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Glasses", "Cap", "Necklace"],
-        },
-        {
-          name: "Food",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Fruits", "Vegetables", "Meat"],
-        },
-        {
-          name: "Electronics",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Smartphone", "Laptop", "Headphones"],
-        },
-        {
-          name: "Books",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Fiction", "Non-Fiction", "Self-Help"],
-        },
-        {
-          name: "Sports",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Ball", "Racket", "Tennis"],
-        },
-        { name: "Home", expanded: false, objects: ["Sofa", "Table", "Chair"] },
-        {
-          name: "Beauty",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Makeup", "Cream", "Perfume"],
-        },
-        {
-          name: "Toys",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Doll", "Car", "Ball"],
-        },
-        {
-          name: "Games",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Board Game", "Cards", "Video Game"],
-        },
-        {
-          name: "Tools",
-          expanded: false,
-          cssCheckbox: "svgArrow",
-          objects: ["Hammer", "Screwdriver", "Saw"],
-        },
-      ],
+    return {     
+      groups: this.value, 
       selectedOptions: [],
       searchTerm: "",
       showDropdownMenu: false,
@@ -122,10 +70,9 @@ var VuetiSelect = Vue.component("VuetiSelect", {
       },
       cssUlBox: "",
       cssDropdownMenu: "dropdownMenu",
-      cssCheckbox: "svgArrow closed",
       isMouseOver: false,
     };
-  },
+  },  
   computed: {
     filteredOptions() {
       return this.groups.filter(
@@ -162,9 +109,9 @@ var VuetiSelect = Vue.component("VuetiSelect", {
 
     toggleGroup(group) {
       group.expanded = !group.expanded;
-      if(group.expanded){
+      if (group.expanded) {
         group.cssCheckbox = "svgArrow open";
-      } else{
+      } else {
         group.cssCheckbox = "svgArrow";
       }
     },
